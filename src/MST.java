3d48051b1c629e5b83a6275c70180aa38a20f7d6
ClayBenson94 <cmb3602@g.rit.edu>
2016-02-11 18:00:12 -0500

    Added a basic representation of an adjacency matrix. Code is still messy, but the basic structure is there (Nested ArrayLists)

--- src/MST.java
+++ src/MST.java
@@ -1,49 +1,11 @@
 import graphObjects.Edge;
 import graphObjects.Vertex;
+import graphObjects.Graph;
 import java.util.ArrayList;
 
 public class MST {
 
     public static void main(String[] args) {
-        //Crappy OO Version of representing a graph?
-//        Vertex myVertex = new Vertex(10);
-//        Vertex myOtherVertex = new Vertex(12);
-//        Edge myCrapEdge = new Edge(myVertex, myOtherVertex);
-//        System.out.println(myVertex);
-//        System.out.println(myCrapEdge);
-
-        //Adjacency List representation
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<ArrayList<Integer>>();
-
-        //Add node 0
-        ArrayList<Integer> adjListRow = new ArrayList<Integer>();
-        adjListRow.add(1);
-        adjListRow.add(3);
-        adjList.add(adjListRow);
-
-        //Add node 1
-        adjListRow = new ArrayList<Integer>();
-        adjListRow.add(0);
-        adjList.add(adjListRow);
-
-        //Add node 2
-        adjListRow = new ArrayList<Integer>();
-        //adjListRow.add(0);
-        adjList.add(adjListRow);
-
-        //Add node 3
-        adjListRow = new ArrayList<Integer>();
-        adjListRow.add(0);
-        adjList.add(adjListRow);
-
-        for (int i = 0; i < adjList.size(); ++i) {
-            System.out.print(String.format("The node %d has adjacency: ",i));
-            for (int j = 0; j < adjList.get(i).size(); ++j) {
-                System.out.print(adjList.get(i).get(j));
-                System.out.print(" ");
-            }
-            System.out.println("");
-        }
-
+        Graph myGraph = new Graph(8);
     }
 }
---
+++ src/graphObjects/Graph.java
@@ -0,0 +1,48 @@
+package graphObjects;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by Clay on 2/11/2016.
+ */
+public class Graph {
+
+    private ArrayList<ArrayList<Integer>> adjMatrix;
+
+    public Graph(int n) {
+        System.out.println(String.format("Making graphs of size %d!\n",n));
+
+        adjMatrix = new ArrayList<ArrayList<Integer>>();
+
+        makeAdjMatrix(n);
+    }
+
+    public ArrayList<ArrayList<Integer>> makeAdjMatrix(int n) {
+
+        //Initialize the matrix to all zeroes to begin
+        for (int i = 0; i < n; ++i) {
+            adjMatrix.add(new ArrayList<Integer>(Collections.nCopies(n, 0))); //Add a blank row (zeroes)
+        }
+
+        //Fill the matrix with random values
+        Random randGen = new Random();
+        for (int column = 0; column < n; ++column) {
+            for (int row = 0; row < n; ++row) {
+                //Introduce random (p) chance here, right now we just always add
+                int randomNum = randGen.nextInt(n) + 1 ;
+                adjMatrix.get(row).set(column, randomNum);
+            }
+        }
+
+        //Print the matrix
+        for (int i = 0; i < n; ++i) {
+            System.out.println(adjMatrix.get(i));
+        }
+
+        return adjMatrix;
+    }
+}
