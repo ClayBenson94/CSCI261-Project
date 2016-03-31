451cdebc62cbd39e05413c9a3684019d18bc5b87
ClayBenson94 <cmb3602@g.rit.edu>
2016-03-31 12:01:17 -0400

    Basic (messy, but functional) Kruskal's algorithm!

---
+++ src/Vertex.java
@@ -0,0 +1,48 @@
+/**
+ * Created by Clay on 3/31/2016.
+ */
+public class Vertex {
+
+    private int index;
+    private Vertex parent;
+    private int rank;
+
+    public Vertex(int index, Vertex parent, int rank) {
+        this.index = index;
+        this.parent = parent;
+        this.rank = rank;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Vertex getParent() {
+        return parent;
+    }
+
+    public void setParent(Vertex parent) {
+        this.parent = parent;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d(%d)",
+                this.getIndex(),
+                this.getParent().getIndex()
+        );
+    }
+
+}
